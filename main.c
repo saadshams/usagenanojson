63ed92c46e83585ce68dedaa4fbcd9d153910797
Saad <saadmuizz@gmail.com>
2023-11-14 01:18:39 -0500

    usage

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+    char *name;
+    int age;
+} person;
+
+void callback(const char *key, const char *value, const char *parentKey) {
+    if (strcmp(key, "name") == 0) person.name = strdup(value);
+    if (strcmp(key, "age") == 0) person.age = atoi(value);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    char *json = "{\"name\": \"John Doe\", \"age\": 25}";
+    // nanojson_parse_object(json, callback, NULL);
+    printf("Name: %s, Age: %d", person.name, person.age); // Name: John Doe, Age: 25
     return 0;
 }
